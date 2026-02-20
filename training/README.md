a8588221479db797636c9fe423af15096f84d58a
Om Anand <82884162+Om-anand-0@users.noreply.github.com>
2026-02-20 12:49:05 +0530

    Delete training directory

--- training/README.md
+++
@@ -1,1 +0,0 @@
-# Flask_Training_Project
--- training/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'you-will-never-guess-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/realtime_chat.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'app/static/images'
